593c634aa00fa4bb69ad43b365dcb930616db9ee
Sandra Llanos <sandra.llanos@xpo.com>
2024-01-15 19:20:27 -0500

    events

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,26 @@
 import PropTypes from 'prop-types'
+
+// if you do not need value inside your function,
+//  it is a good practice to handle functions outside the functional component
+const handleAdd = (event) => console.log(event)
+// function handleAdd(event){
+//     console.log(event);
+// }
+
 export const CounterApp = ({ value }) => {
-    console.log('value :>> ', value);
+    // console.log('value :>> ', value);
     return ( 
         <>
-        <h1>CounterApp</h1>
-        <h2> { value } </h2>
+            <h1>CounterApp</h1>
+            <h2> { value } </h2>
+
+            <button onClick={ handleAdd }>
+            {/* <button onClick={ (event) => handleAdd(event) }> */}
+                +1
+            </button>
+            <button>-1</button>
+            <input onFocus={(e) =>{console.log('Focused on Input', e)}}/>
+            <input onBlur={(e) =>{console.log('Triggered because this input lost focus', e)}}/>
         </>
      );
 }
